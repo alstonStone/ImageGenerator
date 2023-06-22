9181b20cd8ef93dd100e8bc9307269d03ab318fd
alstonStone <alstonStone0@gmail.com>
2023-06-21 22:44:31 -0400

    refactor

--- ImageGenerator/src/main/java/com/alston/Main.java
+++ ImageGenerator/src/main/java/com/alston/Main.java
@@ -1,18 +1,9 @@
 package com.alston;
 
-import com.alston.Model.Rug;
 import com.alston.Service.Artist;
-import com.alston.Service.Pallet;
 import com.alston.Service.RandomNumberJesus;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.util.Random;
-
-import javax.imageio.ImageIO;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -25,7 +16,7 @@ public class Main {
 //        int[] testingNumbers = new int[]{0, 4, 2, 0, 9, 9, 9};
 //        Artist artist = new Artist(300*3,600*3,testingNumbers);
 
-        Artist artist = new Artist(300,500, rnj.getNumbers());
+        Artist artist = new Artist(300*3,500*3, rnj.getNumbers());
         artist.drawRug();
         //artist.addStats();
         artist.saveImage();
